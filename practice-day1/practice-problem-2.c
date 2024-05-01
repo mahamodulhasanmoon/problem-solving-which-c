76a863806026612ee4ea4b73174ad479b8aca4eb
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-05-01 14:37:41 +0600

    fix:basic print problem solved

---
+++ practice-day1/practice-problem-2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * ======== === === === ========================================
+ * === === --------- Problem 1 --------- === ===
+
+You need to take two integer values as input and show the summation, subtraction, multiplication and division in the given format below.
+
+For example:
+Inputs are 5 and 2
+Then you’ll give output as:
+5 + 2 = 7
+5 - 2 = 3
+5 * 2 = 10
+5 / 2 = 2.50
+
+
+ * ===========================================================
+*/
+
+int main(){
+
+int a, b;
+
+scanf("%d %d",&a, &b);
+  
+
+    printf("%d,%d",a,b);
+    return 0;
+}
